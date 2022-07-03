34f5034d5fa35030b512ccbe68c60e3b6bf91ffd
MacBook <macbook@Gabriel.local>
2022-07-03 01:58:26 -0300

    nuevo componente ItemListContainer agregado a App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/Nav';
+import ItemListContainer from './Components/ItemListContainer';
+
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      
+      <ItemListContainer/>
     </div>
   );
 }
---
+++ src/Components/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+const ItemListContainer = () => {
+    return(
+        <>
+        {<h2>Bienvenido a mi futura tienda online!</h2>}
+        </>
+    )
+}
+
+export default ItemListContainer;
