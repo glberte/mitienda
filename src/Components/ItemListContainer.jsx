a79fcd2bc9d99ebcb081d775cb0317aa48f850c4
MacBook <macbook@Gabriel.local>
2022-08-08 00:24:48 -0300

    fix objetos en itemlistcontainer oh

--- src/Components/ItemListContainer.jsx
+++ src/Components/ItemListContainer.jsx
@@ -19,22 +19,19 @@ const ItemListContainer = () => {
     }
     
     useEffect(()=> {
-      // const db = getFirestore(); 
-      // const docRef = doc(db, "items", "2gQmkKlgswrM4gPUmSrj");
-      // getDoc(docRef).then((snapshot)=> {
-      //    const data = {id: snapshot.id, ...snapshot.data()};
-      //    console.log(snapshot.data());
-      //    setItems(snapshot.data())
-      //  },[]);
-
-      // const db= getFirestore();
-      // const itemsCollection = collection(db, "items");
-      // getDocs(itemsCollection).then((snapshot) => {
-      //   console.log(snapshot.docs.map(doc => {
-      //     const data ={id: doc.id, ...doc.data()}
-      //   }));
+      
+      const db= getFirestore();
+      const itemsCollection = collection(db, 'items');
+      getDocs(itemsCollection)
+          .then((snapshot) => console.log(snapshot.docs)
+      );
+      
+      //  getDocs(itemsCollection).then((snapshot) => {
+      //    console.log(snapshot.docs.map(doc => {
+      //      const data ={id: doc.id, ...doc.data()}
+      //    }));
         
-      // })
+      // });
 
       // const db = getFirestore();
       // const itemsCollection = collection(db,"items");
@@ -45,21 +42,24 @@ const ItemListContainer = () => {
       //   );
       //   getDocs(filteredCollection).then((snapshot)=> {})
 
-        let promise = new Promise ((resolve, reject) =>{
-            setTimeout(()=> {
-                resolve(productsDB)
-            }, 2000);
-        })
-        promise.then((productsDB)=>{
-          if (name) {
-            setItems(productsDB.filter((product) => product.category == name));
-          } else{
-            setItems(productsDB)
-          }
-        }).catch(
-             (err)=> console.log(err)
-          );
-    },[name]);
+
+    //  let promise = new Promise ((resolve, reject) =>{
+    //      setTimeout(()=> {
+    //          resolve(productsDB)
+    //      }, 2000);
+    //  });
+
+    //      promise.then((productsDB)=>{
+    //        if (name) {
+    //          setItems(productsDB.filter((product) => product.category == name));
+    //        } else{
+    //          setItems(productsDB)
+    //        }
+    //      }).catch(
+    //           (err)=> console.log(err)
+    //        );
+
+   },[name]);
 
     return(
         <>
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -5,26 +5,26 @@ import BarraFiltros from "./Filter";
 const NavBar = ({amountItems}) => {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg bg-light">
+      <nav className="navbar navbar-expand-lg bg-light">
   <div class="container">
     <Link to={"/"}> <h2>E-Pups</h2> </Link>
     
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
 
-        <li class="nav-item">
+        <li className="nav-item">
           <Link to={"/"}>
-            <a class="nav-link" href="#">Productos</a>
+            <a className="nav-link" href="#">Productos</a>
           </Link>
         </li>
 
-        <li class="nav-item">
+        <li className="nav-item">
           <Link to={"/Contacto"}> 
-          { <a  class="nav-link" href="#">Contacto</a>} 
+          { <a  className="nav-link" href="#">Contacto</a>} 
           </Link>
         </li>
 
